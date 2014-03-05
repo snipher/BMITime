48df73013a956899ee215465d2af386b9f5a675a
Richard <richard@RichardtekiMacBook-Pro.local>
2014-03-05 10:39:40 +0800

    Initial Commit

---
+++ BMITime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BMITime
+//
+//  Created by Richard on 14-3-5.
+//  Copyright (c) 2014年 com.Richard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
